72658977a1b695dd5961157fe59519ceb3ae9ed2
Italo Alves <italo.alves.jp@gmail.com>
2018-06-09 16:34:05 -0300

    -

--- src/model/Answer.java
+++ src/model/Answer.java
@@ -1,21 +1,5 @@
 package model;
 
 
-public class Answer {
-	private String userName;
-	private String text;
-	
-	public String getUserName() {
-		return userName;
-	}
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-	public String getText() {
-		return text;
-	}
-	public void setText(String text) {
-		this.text = text;
-	}
-	
+public class Answer extends DataModel{
 }
